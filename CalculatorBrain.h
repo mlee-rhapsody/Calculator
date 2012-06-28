4b90705d1dc149f07958a65caff9c48fa373b1a9
Miene Lee <mlee@rhapsody.com>
2012-06-27 21:15:22 -0700

    Added model

--- CalculatorBrain.h
+++ CalculatorBrain.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface CalculatorBrain : NSObject
-
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
 @end
--- CalculatorBrain.m
+++ CalculatorBrain.m
@@ -8,6 +8,33 @@
 
 #import "CalculatorBrain.h"
 
-@implementation CalculatorBrain
+@interface CalculatorBrain()
+@property (nonatomic, strong) NSMutableArray *operandStack;
+@end
 
+@implementation CalculatorBrain
+@synthesize operandStack = _operandStack;
+
+- (NSMutableArray *) operandStack{
+    return _operandStack;
+    
+}
+
+- (void)setOperandStack:(NSMutableArray *)operandStack
+{
+    _operandStack = operandStack;
+}
+
+- (void)pushOperand:(double)operand
+{
+    [self.operandStack addObject:[NSNumber numberWithDouble:operand]];
+}
+
+- (double)performOperation:(NSString *)operation
+{
+    double result = 0;
+    
+    // calculate result
+    return result;
+}
 @end
