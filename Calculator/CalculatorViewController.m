8367da9a2efac72e6acfd5c6db36ac195d4c707a
Miene Lee <mlee@rhapsody.com>
2012-06-27 20:38:29 -0700

    Buttons logic done & add model

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -8,14 +8,35 @@
 
 #import "CalculatorViewController.h"
 
+@interface CalculatorViewController()
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+
+@end
+
 @interface CalculatorViewController ()
 
 @end
 
 @implementation CalculatorViewController
-@synthesize display;
+@synthesize display = _display;
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
 
 
-- (IBAction)digitPressed:(UIButton *)sender {
+- (IBAction)digitPressed:(UIButton *)sender 
+{
+    NSString *digit = sender.currentTitle;
+    if(self.userIsInTheMiddleOfEnteringANumber){
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    }else{
+        self.userIsInTheMiddleOfEnteringANumber = true;
+        self.display.text = digit;
+    }
+    
+}
+
+- (IBAction)enterPressed:(UIButton *)sender {
+}
+
+- (IBAction)operationPressed:(UIButton *)sender {
 }
 @end
---
+++ CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by user 1 on 6/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
