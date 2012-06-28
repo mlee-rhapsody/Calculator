3bce34007434e3fcebfd711a0cc4562c55ce207d
Miene Lee <mlee@rhapsody.com>
2012-06-27 23:11:05 -0700

    working calculator via walkthrough

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,20 +7,26 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
-
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
-@interface CalculatorViewController ()
-
-@end
 
 @implementation CalculatorViewController
 @synthesize display = _display;
 @synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+@synthesize brain = _brain;
 
+- (CalculatorBrain *)brain
+{
+    if(_brain == nil){
+        _brain = [[CalculatorBrain alloc] init];
+    }
+    return _brain;
+}
 
 - (IBAction)digitPressed:(UIButton *)sender 
 {
@@ -34,9 +40,20 @@
     
 }
 
-- (IBAction)enterPressed:(UIButton *)sender {
-}
+- (IBAction)enterPressed 
+{
 
-- (IBAction)operationPressed:(UIButton *)sender {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+ }
+
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    if(self.userIsInTheMiddleOfEnteringANumber){
+        [self enterPressed];
+    }
+    double result = [self.brain performOperation:sender.currentTitle];
+    NSString *resultString = [NSString stringWithFormat:@"%g", result];
+    self.display.text = resultString;
 }
 @end
--- CalculatorBrain.m
+++ CalculatorBrain.m
@@ -16,13 +16,11 @@
 @synthesize operandStack = _operandStack;
 
 - (NSMutableArray *) operandStack{
+    if(_operandStack == nil){
+        _operandStack = [[NSMutableArray alloc] init];
+    }
     return _operandStack;
     
-}
-
-- (void)setOperandStack:(NSMutableArray *)operandStack
-{
-    _operandStack = operandStack;
 }
 
 - (void)pushOperand:(double)operand
@@ -30,11 +28,31 @@
     [self.operandStack addObject:[NSNumber numberWithDouble:operand]];
 }
 
+- (double)popOperand
+{
+    NSNumber *operandObject = [self.operandStack lastObject];
+    if(operandObject){
+        [self.operandStack removeLastObject];
+    }
+    
+    return [operandObject doubleValue];
+}
+
 - (double)performOperation:(NSString *)operation
 {
     double result = 0;
     
-    // calculate result
+    if([operation isEqualToString:@"+"]){
+        result = [self popOperand] + [self popOperand];
+    }else if([@"*" isEqualToString:operation]){
+        result = [self popOperand] * [self popOperand];
+    }else if([@"/" isEqualToString:operation]){
+        result = [self popOperand] / [self popOperand];
+    }else if([@"-" isEqualToString:operation]){
+        result = [self popOperand] - [self popOperand];
+    }
+    
+    [self pushOperand:result];
     return result;
 }
 @end
