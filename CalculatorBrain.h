b0ece855d8a0ae4f13aa8cc2695fe197d1dfda62
Miene Lee <mlee@rhapsody.com>
2012-07-08 17:14:56 -0700

    Ass2 walthrough

--- CalculatorBrain.h
+++ CalculatorBrain.h
@@ -12,4 +12,10 @@
 - (void)pushOperand:(double)operand;
 - (double)performOperation:(NSString *)operation;
 - (void)clearAll;
+
+@property (readonly) id program;
+
++(double)runProgram:(id)program;
++(NSString *)descriptionOfProgram:(id)program;
+
 @end
